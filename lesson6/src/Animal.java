7a948f6f0b8d30113f5923720b879bd8fe6a0eac
fireflysmk <fireflysmk@gmail.com>
2021-06-27 13:02:35 +0300

    lesson06

---
+++ lesson6/src/Animal.java
@@ -0,0 +1,96 @@
+
+/*
+2. Все животные могут бежать и плыть. В качестве параметра каждому методу передается длина препятствия.
+Результатом выполнения действия будет печать в консоль. (Например, dogBobik.run(150); -> 'Бобик пробежал 150 м.');
+3. У каждого животного есть ограничения на действия (бег: кот 200 м., собака 500 м.; плавание: кот не умеет плавать, собака 10 м.).
+4. * Добавить подсчет созданных котов, собак и животных.
+
+ */
+
+public abstract class Animal {
+
+    protected String name;
+    protected int maxRunDistance;
+    protected int maxSwimDistance;
+    private static int animalCounter;
+
+    public Animal(String name) {
+        this.name = name;
+        incrimentAnimalCounter();
+    }
+
+    public int getAnimalCounter() {
+        return animalCounter;
+    }
+
+    public void incrimentAnimalCounter(){
+        animalCounter++;
+    }
+
+    public void decrimentAnimalCounter(){
+        animalCounter--;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void run(int distance) {
+
+        if (Math.abs(distance) <= maxRunDistance) {
+            System.out.println(this.name + " пробежал " + distance +" м.");
+        } else {
+            System.out.println(this.name + " пробежал из " + distance + "м, только " + maxRunDistance + "м. и прилег отдохнуть");
+        }
+    }
+
+    public void swim (int distance) {
+
+        if (maxSwimDistance == 0) {
+            System.out.println("Злая судьба направила бедного " + this.name + " в речку и он утонул" +
+                    "\n (играет грустная музыка)");
+            decrimentAnimalCounter();
+        } else if (Math.abs(distance) <= maxSwimDistance) {
+            System.out.println(this.name + " проплыл " + distance +" м.");
+        } else {
+            System.out.println(this.name + " проплыл из " + distance + "м. только " + maxSwimDistance + "м. и пошел ко дну" +
+                    "\n (играет грустная музыка)");
+            decrimentAnimalCounter();
+        }
+    }
+
+    public <T> boolean overcomeObstacles(T obtacles){
+        int obtDist;
+
+        if (obtacles instanceof Road) {
+            obtDist = ((Road) obtacles).getDistance();
+            //System.out.println("Road " +  obtDist + " м");
+            if (obtDist <= maxRunDistance) {
+                System.out.println(name + " прошел препятствие Road (" + obtDist + "м.)");
+                return true;
+            } else {
+                System.out.println(name + " не смог преодолеть препятствие Road(" + obtDist + "м.)");
+            }
+        } else if (obtacles instanceof River) {
+            obtDist = ((River) obtacles).getDistance();
+            //System.out.println("River " +  obtDist + " м");
+            if (obtDist <= maxSwimDistance) {
+                System.out.println(name + " прошел препятствие River (" + obtDist + "м.)");
+                return true;
+            } else {
+                System.out.println(name + " не смог преодолеть препятствие River(" + obtDist + "м.)");
+            }
+        }
+        return false;
+    }
+
+    /*
+
+    5 **** Сделать классы препятствий дорога и водоём. У них будет длина.
+    Сделать в животных методы преодоления препятствий.
+    Сделать массив разных животных и массив препятствий.
+    Заставить животных пройти все препятствия, причем если животное не преодолело одно из - то оно выбывает и дальше не пробует.
+     */
+
+
+}
---
+++ lesson6/src/Obtacles.java
@@ -0,0 +1,12 @@
+public abstract class Obtacles {
+
+    private int distance;
+
+    public Obtacles (int distance) {
+        this.distance = distance;
+    }
+
+    public int getDistance() {
+        return this.distance;
+    }
+}
